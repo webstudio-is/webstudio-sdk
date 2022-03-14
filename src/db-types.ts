549db641e13e8657eb8df2c9a3b46e288567af0b
Oleg Isonen <oleg008@gmail.com>
2022-03-14 23:32:11 +0000

    add generated prisma client to npm package

--- src/db-types.ts
+++ src/db-types.ts
@@ -1,4 +1,4 @@
-export type { InstanceProps, Project } from "@prisma/client";
+export type { InstanceProps, Project } from "../generated/prisma-client";
 import type { Instance } from "./instance";
 export type Tree = {
   id: string;
