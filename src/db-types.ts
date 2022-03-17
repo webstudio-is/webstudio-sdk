830599e9f4696998eddc5af5746e4b46c201d3ad
Oleg Isonen <oleg008@gmail.com>
2022-03-17 16:18:26 +0000

    export user type

--- src/db-types.ts
+++ src/db-types.ts
@@ -1,4 +1,4 @@
-export type { InstanceProps, Project } from "../generated/prisma-client";
+export type { InstanceProps, Project, User } from "../generated/prisma-client";
 import type { Instance } from "./instance";
 export type Tree = {
   id: string;
