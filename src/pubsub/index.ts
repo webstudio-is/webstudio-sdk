bb0961f3cf71c79ab02f0d03b69bdea6bfc02d56
Oleg Isonen <oleg008@gmail.com>
2022-03-25 17:24:21 +0000

    use emitter in useSubscribe

--- src/pubsub/index.ts
+++ src/pubsub/index.ts
@@ -1,20 +1,36 @@
+import mitt from "mitt";
 import { useEffect } from "react";
 
-// @todo this is going to be highly inefficient, because we will
-// add and remove event listener gazillion of times.
-export const useSubscribe = <Type, Payload = undefined>(
+// @todo do we want to have a central map of all events/data?
+// Pro - you can't use a wrong event with a wrong data
+// Contra - if we have hundreds of events, this list will become a central thing to change,
+// may result in maintenance downsides and merge conflicts, because it will have to import each data type
+type Events = Record<string, any>;
+
+export const emitter = mitt<Events>();
+
+if (typeof window === "object") {
+  window.addEventListener(
+    "message",
+    (event: MessageEvent) => {
+      // @todo this has no type safety built in, could be anything from any source.
+      // we could potentially maintain a list of valid event types, but see prior contra points.
+      if (typeof event.data?.type === "string") {
+        emitter.emit(event.data.type, event.data.payload);
+      }
+    },
+    false
+  );
+}
+
+export const useSubscribe = <Type extends string, Payload = undefined>(
   type: Type,
   onAction: (payload: Payload) => void
 ) => {
   useEffect(() => {
-    const handleMessage = (event: MessageEvent) => {
-      if (event.data?.type === type) {
-        onAction(event.data.payload);
-      }
-    };
-    window.addEventListener("message", handleMessage, false);
+    emitter.on(type, onAction);
     return () => {
-      window.removeEventListener("message", handleMessage);
+      emitter.off(type, onAction);
     };
   }, [type, onAction]);
 };
