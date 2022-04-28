26e373ca8e5e81922628a4804e2820a1f5e2a559
Oleg Isonen <oleg008@gmail.com>
2022-04-29 00:00:42 +0100

    new breakpoints sort order

--- src/css/breakpoints.ts
+++ src/css/breakpoints.ts
@@ -3,9 +3,9 @@ import { type Breakpoint } from "../db-types";
 export type BaseBreakpoint = Pick<Breakpoint, "label" | "minWidth">;
 
 export const initialBreakpoints: Array<BaseBreakpoint> = [
-  { label: "Desktop", minWidth: 1280 },
-  { label: "Laptop", minWidth: 1024 },
-  { label: "Tablet", minWidth: 768 },
-  { label: "Mobile", minWidth: 360 },
   { label: "Default", minWidth: 0 },
+  { label: "Mobile", minWidth: 360 },
+  { label: "Tablet", minWidth: 768 },
+  { label: "Laptop", minWidth: 1024 },
+  { label: "Desktop", minWidth: 1280 },
 ];
