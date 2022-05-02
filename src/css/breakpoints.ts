8d478f1c5ba55a998d85943b50573a8ff7683737
Oleg Isonen <oleg008@gmail.com>
2022-05-02 13:49:07 +0200

    remove default breakpoint, mobile starts at 0 now - more obvious that people should develop mobile first

--- src/css/breakpoints.ts
+++ src/css/breakpoints.ts
@@ -3,8 +3,7 @@ import { type Breakpoint } from "../db-types";
 export type BaseBreakpoint = Pick<Breakpoint, "label" | "minWidth">;
 
 export const initialBreakpoints: Array<BaseBreakpoint> = [
-  { label: "Default", minWidth: 0 },
-  { label: "Mobile", minWidth: 360 },
+  { label: "Mobile", minWidth: 0 },
   { label: "Tablet", minWidth: 768 },
   { label: "Laptop", minWidth: 1024 },
   { label: "Desktop", minWidth: 1280 },
