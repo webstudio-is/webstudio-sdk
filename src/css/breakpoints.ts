4279a08ddb81dc7100c5d2c5b774783432782a31
Oleg Isonen <oleg008@gmail.com>
2022-05-10 23:04:14 +0200

    decided to start mobile breakpoint from 360, there is no reason to style anything on 0
    it was a bit awkward that lower end of mobile breakpoint is a special case for lower bound constraint on canvas resizing

--- src/css/breakpoints.ts
+++ src/css/breakpoints.ts
@@ -3,7 +3,7 @@ import { type Breakpoint } from "../db-types";
 export type BaseBreakpoint = Pick<Breakpoint, "label" | "minWidth">;
 
 export const initialBreakpoints: Array<BaseBreakpoint> = [
-  { label: "Mobile", minWidth: 0 },
+  { label: "Mobile", minWidth: 360 },
   { label: "Tablet", minWidth: 768 },
   { label: "Laptop", minWidth: 1024 },
   { label: "Desktop", minWidth: 1280 },
