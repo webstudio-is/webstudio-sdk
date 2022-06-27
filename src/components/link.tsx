285195db8a9bcfe0d132ddfc94602ea891e197e2
Andrey Okonetchnikov <andrey@okonet.dev>
2022-06-27 15:01:35 +0200

    fix(Link): Make `href` optional
    Right now we don't have an ability to prevent a component from
    rendering if the required prop isn't provided. To work-around this
    the `href` is made optional again but in the future we'd need a
    more general solution to handle required props.

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -3,12 +3,12 @@ import React, { forwardRef, type ElementRef, type ComponentProps } from "react";
 const defaultTag = "a";
 
 export type LinkProps = Omit<ComponentProps<typeof defaultTag>, "href"> & {
-    href: string;
+    href?: string;
 }
 
 export const Link = forwardRef<
   ElementRef<typeof defaultTag>,
     LinkProps
->((props, ref) => <a {...props} ref={ref} />);
+>(({href = '', ...props}, ref) => <a {...props} href={href} ref={ref} />);
 
 Link.displayName = "Link";
